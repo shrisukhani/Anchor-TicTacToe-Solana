95988a046ed908c0ef9731542a0e0f221bd2ac25
Shridhar Sukhani <sukha014@umn.edu>
2022-01-12 23:59:16 -0800

    Initial Commit

---
+++ tests/tic-tac-toe.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { TicTacToe } from '../target/types/tic_tac_toe';
+
+describe('tic-tac-toe', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
